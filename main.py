fc16896270277af6ddd4c10b607dc3c884794c79
thisisanro <antonromanenko@tutanota.com>
2025-02-23 12:25:56 +0000

    Add functionality to read user-specified text file via command-line argument

--- main.py
+++ main.py
@@ -1,52 +1,41 @@
+import sys
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
+
+
 def main():
-    book_path = 'books/frankenstein.txt'
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
+
     text = get_book_text(book_path)
-    number_of_words = count_words(text)
-    number_of_chars = count_characters(text)
-    list_of_chars = list_from_dictionary(number_of_chars)
-    
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{number_of_words} words found in the document")
-    print("")
-    print_report(list_of_chars)
-    print("--- End report ---")
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
-def print_report(list):
-    for line in list:
-        char = line['char']
-        number = line['number']
-        if char == ' ':
-            char = "space"
-        elif not char.isalpha():
-            continue
-        print(f"The {char} character was found {number} times")
-
-def count_words(text):
-    words = text.split()
-    return len(words)
 
 def get_book_text(path):
     with open(path) as f:
-        return  f.read()
-    
-def count_characters(text):
-    lower_text = text.lower()
-    char_count = {}
-    for char in lower_text:
-        if char in char_count:
-            char_count[char] += 1
-        else:
-            char_count[char] = 1
-    return char_count
+        return f.read()
 
-def list_from_dictionary(dict):
-    result_list = []
-    for k, v in dict.items():
-        result_list.append({"char": k, "number": v})
-    result_list.sort(reverse=True, key=sort_on)
-    return result_list
 
-def sort_on(dict):
-    return dict["number"]
-        
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
 main()
